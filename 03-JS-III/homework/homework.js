2ec64b675e2064938af78dfd9b71646b9fe70259
gabsramz <mariagramirezr@gmail.com>
2022-10-13 23:21:34 -0300

    Homework JSIII final done 20221013

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -20,12 +20,28 @@ function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  paresencontrados = []
+  for (let i = 0; i < array.length; i++) {
+    if (array[i]%2==0) {
+      paresencontrados.push(array[i])
+    }
+  }
+  return paresencontrados;
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  var resultado = array.map(function (elemento) {
+    return elemento ** 2
+  });
+  return resultado;
+  // resultado = []
+  // array.forEach(function (elemento) {
+  //   resultado.push(elemento ** 2)
+  // });
+  // return resultado;
 }
 
 function sumaArray(array){
@@ -44,7 +60,9 @@ function sumaArray(array){
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  return num.toString().length;
 }
+
   
   
   // No modificar nada debajo de esta línea
